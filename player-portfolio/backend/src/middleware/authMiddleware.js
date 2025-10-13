9361af4fede8eb86a115a1fdf9010834e8290334
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-10-13 14:38:14 +0200

    add authmiddleware file for have a verification if use is connected to jwt and if we have the token in the head

---
+++ player-portfolio/backend/src/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    // Vérifier la présence du token dans l’en-tête
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Accès refusé. Aucun token fourni." });
+    }
+
+    // Vérifier la validité du token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Charger l’utilisateur associé
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé." });
+    }
+
+    req.user = user; // Attacher l’utilisateur à la requête
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token invalide ou expiré.", error: error.message });
+  }
+};
